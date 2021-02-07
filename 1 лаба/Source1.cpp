6565226f795b118d8cbd6ec7b8e95765ca9118b2
Alexey Gavshin <agavshin25@gmail.com>
2021-02-07 19:23:19 +0300

    1 lab

---
+++ 1 лаба/Source1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+	double a, b, c;
+	cin >> a >> b;
+	c = sqrt(a * a + b * b);
+	cout << c;
+	return 0;
+}
